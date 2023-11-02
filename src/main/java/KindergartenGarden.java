e2d32153cb3d9640361695df51fd212f02b8613d
Viktor <66754380+Delliraz@users.noreply.github.com>
2023-11-02 01:49:13 +0100

    Initial commit

---
+++ src/main/java/KindergartenGarden.java
@@ -0,0 +1,20 @@
+package tdd;
+
+import java.util.List;
+
+public class KindergartenGarden {
+
+	public KindergartenGarden(String plants) {
+		// TODO: implement
+	}
+
+	public KindergartenGarden(String plants, String[] names) {
+		// TODO: implement
+	}
+
+	public List<Plant> getPlantsOfStudent(String name) {
+		// TODO: implement
+		return null;
+	}
+
+}
